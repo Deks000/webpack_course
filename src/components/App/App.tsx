f504928659792328d3d725698f5845a8af608875
Дмитрий <deman7352@yandex.ru>
2024-06-25 14:23:10 +0300

    25.06beforeMicrfrontend

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,14 +6,20 @@ import logo2Png from '@/assets/Logo2.png'
 import logo1Jpg from '@/assets/Logo1.jpg'
 import Logo3Svg from '@/assets/Logo3.svg'
 
-function TODO(a: number) {
-    console.log('T0D0FUNCTION')
+function TODO() {
+    TODO2()
+
+}function TODO2() {
+    throw new Error();
 }
 
 export const App = () => {
     const [count, setCount] = useState<number>(0);
 
-    const increment = () => setCount(prev => prev + 1)
+    const increment = () => {
+        // setCount(prev => prev + 1)
+        TODO();
+    }
     // TODO('51325')
     // if (__PLATFORM__ === 'desktop') {
     //     return <div>ISDESKTOPPLATFORM</div>
@@ -28,8 +34,8 @@ export const App = () => {
     // }
 
     return (
-        <div>
-            <div>PLATFORM={__PLATFORM__}</div>
+        <div data-testid={'App.DataTestId'}>
+            <h1 data-testid={'Platform'}>PLATFORM={__PLATFORM__}</h1>
             <div>
                 <img width={100} height={100} src={logo2Png} alt=""/>
                 <img width={100} height={100} src={logo1Jpg} alt=""/>
